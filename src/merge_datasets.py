7faeeb8dbfdfb0b7c58b78cffe5b1da0ae1cdec7
Neea Rusch <neearusch@gmail.com>
2022-02-17 00:21:33 -0500

    edit datasets

--- src/merge_datasets.py
+++ src/merge_datasets.py
@@ -1,6 +1,6 @@
 import sys
 import collections
-from random import shuffle
+from random import shuffle, randint
 from datetime import datetime
 from os import path
 
@@ -21,9 +21,11 @@ def normalize_labels(rows, idx):
         row[idx] = (row[idx]).lower()
 
 
+f1 = sys.argv[1]
+f2 = sys.argv[2]
 LABEL = 'label'
-h1, r1 = read_csv(sys.argv[1])
-h2, r2 = read_csv(sys.argv[2])
+h1, r1 = read_csv(f1)
+h2, r2 = read_csv(f2)
 DT_STR = datetime.now().strftime("%Y%m%d_%H%M%S")
 FILENAME = f'dataset_{DT_STR}.csv'
 OUT_DIR = 'data'
@@ -35,25 +37,50 @@ if h1 != h2:
     sys.exit(1)
 
 # merge rows and find index of label column
-ALL_ROWS, LABEL_IDX = r1 + r2, h1.index(LABEL)
+ALL_ROWS, LABEL_IDX = r1, h1.index(LABEL)
 TROWS = len(ALL_ROWS)
 
 # make all labels same letter case
 normalize_labels(ALL_ROWS, LABEL_IDX)
+ALL_ROWS.sort(key=lambda the_list: the_list[LABEL_IDX])
 
 CLASS_LABELS = [r[LABEL_IDX] for r in ALL_ROWS]
 LABEL_FREQ = collections.Counter(CLASS_LABELS)
+MIN_FREQ = min([LABEL_FREQ[label] for label in list(set(CLASS_LABELS))])
+SAMPLED_LIST = []
 
 # print some stats on each label
+print(f'Total rows: {TROWS}')
+print(f'Min freq: {MIN_FREQ}')
+
+for label in list(set(CLASS_LABELS)):
+    freq = LABEL_FREQ[label]
+    perc = 100 * freq / TROWS
+    subset = [record for record in ALL_ROWS if
+              record[LABEL_IDX] == label]
+    RAND_FACTOR = randint(-int(MIN_FREQ * .05), int(MIN_FREQ * .05))
+    shuffle(subset)
+    # take uniformly with some randomness
+    take = subset[:(MIN_FREQ + RAND_FACTOR)]
+    SAMPLED_LIST += take
+
+    print(f'{label.ljust(10)} ',
+          f'{str(freq).rjust(10)} ',
+          f'{perc:.2f} %',
+          f'took: {str(len(take))}'.rjust(20))
+
+TROWS = len(SAMPLED_LIST)
+CLASS_LABELS = [r[LABEL_IDX] for r in SAMPLED_LIST]
+LABEL_FREQ = collections.Counter(CLASS_LABELS)
+shuffle(SAMPLED_LIST)
+
 for label in list(set(CLASS_LABELS)):
     freq = LABEL_FREQ[label]
     perc = 100 * freq / TROWS
     print(f'{label.ljust(10)} ',
-          f'{str(freq).rjust(10)}',
-          f'{perc:.2f}'.rjust(10), '%')
-print(f'Total rows: {TROWS}')
+          f'{str(freq).rjust(10)} ',
+          f'{perc:.2f} %')
 
-shuffle(ALL_ROWS)  # maybe these need to be in chronological order?
-
-save_csv(path.join(OUT_DIR, FILENAME), ALL_ROWS, h1)
+print(f'Total rows after sampling: {TROWS}')
+save_csv(path.join(OUT_DIR, FILENAME), SAMPLED_LIST, h1)
 print('done.')
