46282f648e0c6b5263dfc2b8f20e31eacf5f507d
Neea <neearusch@gmail.com>
2022-08-10 08:35:53 -0400

    refactor utils

--- src/utility.py
+++ src/utility.py
@@ -1,6 +1,3 @@
-from csv import reader, writer
-from typing import Tuple, List
-
 import numpy as np
 import pandas as pd
 from colorama import Fore, Style  # terminal colors
@@ -8,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 DEFAULT_DS = 'data/CTU-1-1.csv'
 """When no dataset is defined, use this one by default."""
-"""We use here a dataset with 50/50 split"""
+"""We use here a dataset with ~ 50/50 split"""
 
 
 def text_label(i):
@@ -104,58 +101,3 @@ def non_bin_attributes(np_array):
     """Get column indices of non-binary attributes"""
     return [feat for feat in range(len(np_array[0]))
             if len(list(set(np_array[:, feat]))) > 2]
-
-
-def read_csv(path: str, delimiter=',') -> Tuple[list, list]:
-    """read some CSV file and split into headers and rows
-
-    Arguments:
-        path: file path to a CSV file
-        delimiter: optional delimiter
-
-    Returns:
-        Tuple where first item is data headers (list)
-        and second is row of data
-    """
-    with open(path, 'r') as read_obj:
-        csv_reader = reader(read_obj, delimiter=delimiter)
-        all_rows = [row for row in csv_reader]
-        header = all_rows[0]
-        rows = all_rows[1:]
-
-    return header, rows
-
-
-def read_lines(path: str) -> List[str]:
-    """Read text file line by line"""
-    with open(path, 'r') as fp:
-        return fp.readlines()
-
-
-def save_csv(filename: str, rows: List[List], headers: List = None):
-    """saves CSV file
-
-    Arguments:
-        filename: where to save file (with path and extension)
-        rows: list of data rows
-        headers: csv file headers (optional)
-    """
-    with open(filename, 'w') as fp:
-        w = writer(fp)
-
-        if headers:
-            w.writerow(headers)
-
-        for row in rows:
-            w.writerow(row)
-
-
-def save_file(filename: str, lines: List[str]):
-    """Write a text file.
-
-    Arguments:
-        filename: where to save file (with path and extension)
-        lines: lines fo text
-    """
-    with open(filename, 'w') as fp:
-        fp.write('\n'.join(lines))
