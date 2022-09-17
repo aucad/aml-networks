13092d258da143dc86ce7b78382c35c8953bcd1c
Neea <neearusch@gmail.com>
2022-09-17 15:39:36 -0400

    edit the validator

--- src/validator.py
+++ src/validator.py
@@ -1,214 +1,270 @@
 from __future__ import annotations
 from random import uniform, randint
+from collections import Counter
 import warnings
 
 import joblib
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 
-warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore")  # ignore feature names warning
+
 VALID, INVALID = 0, 1
 
+TCP_MODEL: DecisionTreeClassifier = joblib.load('data/DT_tcp.sav')
+UDP_MODEL: DecisionTreeClassifier = joblib.load('data/DT_udp.sav')
 
-class NbProto:
+TCP_ATTR = TCP_MODEL.feature_names_in_
+UDP_ATTR = UDP_MODEL.feature_names_in_
 
-    def __init__(self, name=None, path=None, **kwargs):
+
+class NetworkProto:
+    """Represents validatable instance"""
+
+    def __init__(self, name, **kwargs):
         self.name = name
-        self.model_path = path
         self.record = {}
-        for key in self.attributes:
-            self.record[key] = 0 if key not in kwargs else kwargs[key]
+        self.set_record(**kwargs)
 
     @property
     def attributes(self):
         return []
 
     @property
+    def values(self):
+        return list(self.record.values())
+
+    @property
     def is_valid(self):
-        return False
+        return INVALID
 
-    @staticmethod
-    def random_info() -> NbProto:
-        return NbProto()
+    def generate(self):
+        return self
 
-    def to_record(self, *values):
-        return dict([(a, b) for a, b in zip(self.attributes, values)])
+    def set_record(self, **kwargs):
+        self.record = dict(
+            [(key, 0 if key not in kwargs else kwargs[key])
+             for key in self.attributes])
 
     def check(self, model: DecisionTreeClassifier):
         if not self.is_valid:
             return INVALID
-        model_attr = list(model.feature_names_in_)
-        rec_attr = list(self.record.keys())
-        values = [self.record[k] if k in self.record else 0
-                  for k in model_attr] \
-            if model_attr != rec_attr else self.record.values()
-        return model.predict(np.array(values).reshape(1, -1))[0]
+        return model.predict(np.array(self.values).reshape(1, -1))[0]
 
 
-class NbTCP(NbProto):
-    def __init__(self, classifier='data/DT_tcp.sav', **kwargs):
-        super().__init__('tcp', classifier, **kwargs)
+class NBRecord(NetworkProto):
+    """Model for NB15 instance"""
+
+    @property
+    def swin(self):
+        return self.record['swin']
+
+    @property
+    def dwin(self):
+        return self.record['dwin']
+
+    @property
+    def synack(self):
+        return self.record['synack']
+
+    @property
+    def ackdat(self):
+        return self.record['ackdat']
+
+    @property
+    def tcprtt(self):
+        return self.record['tcprtt']
+
+    @property
+    def dbytes(self):
+        return self.record['dbytes']
+
+    @property
+    def Dload(self):
+        return self.record['Dload']
+
+    @property
+    def dttl(self):
+        return self.record['dttl']
+
+    @property
+    def Djit(self):
+        return self.record['Djit']
+
+    @property
+    def Dpkts(self):
+        return self.record['Dpkts']
+
+    @property
+    def smeansz(self):
+        return self.record['smeansz']
+
+    @property
+    def Spkts(self):
+        return self.record['Spkts']
+
+    @property
+    def sbytes(self):
+        return self.record['sbytes']
+
+    @property
+    def dmeansz(self):
+        return self.record['dmeansz']
+
+    @property
+    def Dpkts(self):
+        return self.record['Dpkts']
+
+    @property
+    def dbytes(self):
+        return self.record['dbytes']
+
+
+class NbTCP(NBRecord):
+    def __init__(self, **kwargs):
+        super().__init__('NB15_tcp', **kwargs)
 
     @property
     def attributes(self):
-        return ('sbytes,dbytes,sttl,dttl,sloss,dloss,Sload,Dload,'
-                'Spkts,Dpkts,swin,dwin,stcpb,dtcpb,smeansz,dmeansz,'
-                'trans_depth,res_bdy_len,Sjit,Djit,Sintpkt,Dintpkt,'
-                'tcprtt,synack,ackdat,is_sm_ips_ports,ct_state_ttl,'
-                'ct_flw_http_mthd,is_ftp_login,ct_ftp_cmd,ct_srv_src,'
-                'ct_srv_dst,ct_dst_ltm,ct_src_ ltm,ct_src_dport_ltm,'
-                'ct_dst_sport_ltm,ct_dst_src_ltm').split(',')
+        return TCP_ATTR
 
     @property
     def is_valid(self):
-        # 0: valid 1: invalid
-        print(type(self.record['synack']))
-        if str(self.record['swin']) != str(255) or \
-                str(self.record['dwin']) != str(255):
-            print('p')
+        if self.swin != 255 or self.dwin != 255:
             return False
-        elif round(float(self.record['synack']) +
-                   float(self.record['ackdat']), 3) != \
-                round(float(self.record['tcprtt']), 3):
-            print('pp')
+        if round(self.synack + self.ackdat, 3) != round(self.tcprtt, 3):
             return False
-        elif self.record['dbytes'] == 0 and \
-                (self.record['Dload'] != 0 or self.record[
-                    'dttl'] != 0 or
-                 self.record['Djit'] != 0 or self.record['Dpkts'] != 0):
-            print('ppp')
+        if self.dbytes == 0 and (
+                self.Dload != 0 or self.dttl != 0 or
+                self.Djit != 0 or self.Dpkts != 0):
             return False
         return True
 
-    def random_info(self):
-        x = uniform(0, 1) < 0.5
-        y = uniform(0, 1) < 0.5
+    def generate(self):
+        """Generates random NB15 TCP record"""
+        p, q = uniform(0, 1) < 0.5, uniform(0, 1) < 0.5
         ackdat = uniform(0.0, 5.512234)
         synack = uniform(0.0, 4.525272)
+        self.set_record(
+            sbytes=randint(0, 14355774),
+            dbytes=0 if q else randint(0, 14657531),
+            sttl=randint(0, 255),
+            dttl=0 if q else randint(0, 254),
+            sloss=randint(0, 5319),
+            dloss=randint(0, 5507),
+            Sload=uniform(0, 5988000256.0),
+            Dload=0 if q else uniform(0, 128761904.0),
+            Spkts=randint(0, 10646),
+            Dpkts=0 if q else randint(0, 11018),
+            swin=255 if p else randint(0, 255),
+            dwin=255 if p else randint(0, 255),
+            stcpb=randint(0, 4294958913),
+            dtcpb=randint(0, 4294953724),
+            smeansz=randint(0, 1504),
+            dmeansz=randint(0, 1500),
+            trans_depth=randint(0, 172),
+            res_bdy_len=randint(0, 6558056),
+            Sjit=uniform(0, 1483830.917),
+            Djit=0 if q else uniform(0, 781221.1183),
+            Sintpkt=uniform(0, 84371.496),
+            Dintpkt=uniform(0.0, 59485.32),
+            tcprtt=(synack + ackdat) if p else uniform(0.0, 10.037506),
+            synack=synack,
+            ackdat=ackdat,
+            is_sm_ips_ports=randint(0, 1),
+            ct_state_ttl=randint(0, 6),
+            ct_flw_http_mthd=randint(0, 36),
+            is_ftp_login=randint(0, 4),
+            ct_ftp_cmd=randint(0, 8),
+            ct_srv_src=randint(1, 67),
+            ct_srv_dst=randint(1, 67),
+            ct_dst_ltm=randint(1, 67),
+            ct_src_ltm=randint(1, 67),
+            ct_src_dport_ltm=randint(1, 67),
+            ct_dst_sport_ltm=randint(1, 60),
+            ct_dst_src_ltm=randint(1, 67))
+        return self
 
-        record = self.to_record(
-            randint(0, 14355774),
-            0 if y else randint(0, 14657531),  # dbytes
-            randint(0, 255),  # sttl
-            0 if y else randint(0, 254),  # dttl
-            randint(0, 5319),  # sloss
-            randint(0, 5507),  # dloss
-            uniform(0, 5988000256.0),
-            0 if y else uniform(0, 128761904.0),
-            randint(0, 10646),
-            0 if y else randint(0, 11018),
-            255 if x else randint(0, 255),
-            255 if x else randint(0, 255),
-            randint(0, 4294958913),
-            randint(0, 4294953724),
-            randint(0, 1504),
-            randint(0, 1500),
-            randint(0, 172),
-            randint(0, 6558056),
-            uniform(0, 1483830.917),
-            0 if y else uniform(0, 781221.1183),
-            uniform(0, 84371.496),
-            uniform(0.0, 59485.32),
-            (synack + ackdat) if x else uniform(0.0, 10.037506),
-            synack,
-            ackdat,
-            randint(0, 1),
-            randint(0, 6),
-            randint(0, 36),
-            randint(0, 4),
-            randint(0, 8),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 60),
-            randint(1, 67))
-        return NbTCP(self.model_path, **record)
 
-
-class NbUDP(NbProto):
-    def __init__(self, classifier='data/DT_udp.sav', **kwargs):
-        super().__init__('udp', classifier, **kwargs)
+class NbUDP(NBRecord):
+    def __init__(self, **kwargs):
+        super().__init__('NB15_udp', **kwargs)
 
     @property
     def attributes(self):
-        return ('sbytes,dbytes,sttl,dttl,sloss,dloss,Sload,Dload,'
-                'Spkts,Dpkts,smeansz,dmeansz,trans_depth,res_bdy_len,'
-                'Suit,Djit,Sintpkt,Dintpkt,is_sm_ips_ports,'
-                'ct_state_ttl,ct_flw_http_mthd,is_ftp_login,'
-                'ct_ftp_cmd,ct_srv_src,ct_srv_dst,ct_dst_ltm,'
-                'ct_src_ ltm,ct_src_dport_ltm,ct_dst_sport_ltm,'
-                'ct_dst_src_ltm').split(',')
+        return UDP_ATTR
 
     @property
     def is_valid(self):
-        # 0: valid 1: invalid
-        return int(self.record['smeansz']) * \
-               int(self.record['Spkts']) == \
-               int(self.record['sbytes']) \
-               and int(self.record['dmeansz']) * \
-               int(self.record['Dpkts']) == \
-               int(self.record['dbytes'])
+        return self.smeansz * self.Spkts == self.sbytes \
+               and self.dmeansz * self.Dpkts == self.dbytes
 
-    def random_info(self):
-        x = uniform(0, 1) < 0.5
-        Smeansz = randint(0, 1504)
-        Dmeansz = randint(0, 1500)
-        Spkts = randint(0, 10646)
-        Dpkts = randint(0, 11018)
-
-        record = self.to_record(
-            Smeansz * Spkts if x else randint(0, 14355774),
-            Dmeansz * Dpkts if x else randint(0, 14657531),
-            randint(0, 255),
-            randint(0, 254),
-            randint(0, 5319),
-            randint(0, 5507),
-            uniform(0, 5988000256.0),
-            uniform(0, 128761904.0),
-            Spkts, Dpkts, Smeansz, Dmeansz,
-            randint(0, 172),
-            randint(0, 6558056),
-            uniform(0, 1483830.917),
-            uniform(0, 781221.1183),
-            uniform(0, 84371.496),
-            uniform(0.0, 59485.32),
-            randint(0, 1),
-            randint(0, 6),
-            randint(0, 36),
-            randint(0, 4),
-            randint(0, 8),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 67),
-            randint(1, 60),
-            randint(1, 67))
-        return NbUDP(self.model_path, **record)
+    def generate(self):
+        """Generates random NB15 UDP record"""
+        r = uniform(0, 1) < 0.5
+        smeansz = randint(0, 1504)
+        dmeansz = randint(0, 1500)
+        spkts = randint(0, 10646)
+        dpkts = randint(0, 11018)
+        self.set_record(
+            sbytes=smeansz * spkts if r else randint(0, 14355774),
+            dbytes=dmeansz * dpkts if r else randint(0, 14657531),
+            sttl=randint(0, 255),
+            dttl=randint(0, 254),
+            sloss=randint(0, 5319),
+            dloss=randint(0, 5507),
+            Sload=uniform(0, 5988000256.0),
+            Dload=uniform(0, 128761904.0),
+            Spkts=spkts,
+            Dpkts=dpkts,
+            smeansz=smeansz,
+            dmeansz=dmeansz,
+            trans_depth=randint(0, 172),
+            res_bdy_len=randint(0, 6558056),
+            Sjit=uniform(0, 1483830.917),
+            Djit=uniform(0, 781221.1183),
+            Sintpkt=uniform(0, 84371.496),
+            Dintpkt=uniform(0.0, 59485.32),
+            is_sm_ips_ports=randint(0, 1),
+            ct_state_ttl=randint(0, 6),
+            ct_flw_http_mthd=randint(0, 36),
+            is_ftp_login=randint(0, 4),
+            ct_ftp_cmd=randint(0, 8),
+            ct_srv_src=randint(1, 67),
+            ct_srv_dst=randint(1, 67),
+            ct_dst_ltm=randint(1, 67),
+            ct_src_ltm=randint(1, 67),
+            ct_src_dport_ltm=randint(1, 67),
+            ct_dst_sport_ltm=randint(1, 60),
+            ct_dst_src_ltm=randint(1, 67))
+        return self
 
 
 class Validator:
 
     def __init__(self):
-        self.models = {}
+        self.models = {'NB15_tcp': TCP_MODEL, 'NB15_udp': UDP_MODEL}
 
-    def get_model(self, record: NbProto):
-        if record.name in self.models:
-            return self.models[record.name]
-        self.models[record.name] = joblib.load(record.model_path)
-        return self.models[record.name]
+    def validate(self, instance: NetworkProto):
+        model = self.models[instance.name]
+        return instance.check(model)
 
-    def validate(self, instance: NbProto):
-        model: DecisionTreeClassifier = self.get_model(instance)
-        result = instance.check(model)
-        print(result, end=' ')
-        return result
+    @staticmethod
+    def random_test(n, proto=None):
+        """Generate and validate N random records"""
+        validator = Validator()
+        item_counter, valid = [], 0
+        generator = lambda: (
+            NbUDP() if proto == 'udp' else NbTCP() if proto == 'tcp'
+            else NbTCP() if randint(0, 1) == 0 else NbUDP())
+        for i in range(n):
+            inst = generator().generate()
+            item_counter.append(inst.name)
+            valid += 1 if validator.validate(inst) == VALID else 0
+
+        counts = [f'{k}: {v}' for k, v in Counter(item_counter).items()]
+        print(f'Generated: {n}, valid: {valid}, {" ".join(counts)}')
 
 
 if __name__ == '__main__':
-    my_validator = Validator()
-    for i in range(100):
-        udp = NbUDP().random_info()
-        my_validator.validate(udp)
+    Validator.random_test(1000)
