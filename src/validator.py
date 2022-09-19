80e7a9c0ef0276d3c4a38cd99bd25a2b67eca7f4
Neea <neearusch@gmail.com>
2022-09-19 10:08:46 -0400

    use namedtuple

--- src/validator.py
+++ src/validator.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from random import uniform, randint
-from collections import Counter
+from collections import Counter, namedtuple
 import warnings
 
 import joblib
@@ -16,35 +16,28 @@ VALID, INVALID = 0, 1
 TCP_MODEL: DecisionTreeClassifier = joblib.load('data/DT_tcp.sav')
 UDP_MODEL: DecisionTreeClassifier = joblib.load('data/DT_udp.sav')
 
-TCP_ATTR = TCP_MODEL.feature_names_in_
-UDP_ATTR = UDP_MODEL.feature_names_in_
+afix = lambda attrs: ",".join([a.replace(' ', '') for a in attrs])
+
+NBTCP = namedtuple('NBTCP', afix(TCP_MODEL.feature_names_in_))
+NBUDP = namedtuple('NBUDP', afix(UDP_MODEL.feature_names_in_))
 
 
 class NetworkProto:
     """Represents validatable instance"""
 
-    def __init__(self, name, **kwargs):
+    def __init__(self, name: str, kind: namedtuple, **kwargs):
         self.name = name
-        self.record = {}
-        self.set_record(**kwargs)
-
-    @property
-    def attributes(self):
-        return []
+        self.attributes = kind._fields or []
+        defaults = dict([(a, 0) for a in self.attributes if a not in kwargs])
+        self.record = kind(**defaults, **kwargs)
 
     @property
     def values(self):
-        return [self.record[a] if a in self.record else 0
-                for a in self.attributes]
+        return [getattr(self.record, a) for a in self.attributes]
 
     @property
     def is_valid(self):
         return INVALID
-
-    def set_record(self, **kwargs):
-        self.record = dict(
-            [(key, 0 if key not in kwargs else kwargs[key])
-             for key in self.attributes])
 
     def check(self, model: DecisionTreeClassifier):
         if not self.is_valid:
@@ -52,118 +45,72 @@ class NetworkProto:
         return model.predict(np.array(self.values).reshape(1, -1))[0]
 
 
-class NBRecord(NetworkProto):
-    """Model for NB15 instance"""
-
-    @property
-    def swin(self):
-        return self.record['swin']
-
-    @property
-    def dwin(self):
-        return self.record['dwin']
-
-    @property
-    def synack(self):
-        return self.record['synack']
-
-    @property
-    def ackdat(self):
-        return self.record['ackdat']
-
-    @property
-    def tcprtt(self):
-        return self.record['tcprtt']
-
-    @property
-    def dbytes(self):
-        return self.record['dbytes']
-
-    @property
-    def Dload(self):
-        return self.record['Dload']
-
-    @property
-    def dttl(self):
-        return self.record['dttl']
-
-    @property
-    def Djit(self):
-        return self.record['Djit']
-
-    @property
-    def Dpkts(self):
-        return self.record['Dpkts']
-
-    @property
-    def smeansz(self):
-        return self.record['smeansz']
-
-    @property
-    def Spkts(self):
-        return self.record['Spkts']
-
-    @property
-    def sbytes(self):
-        return self.record['sbytes']
-
-    @property
-    def dmeansz(self):
-        return self.record['dmeansz']
-
-    @property
-    def Dpkts(self):
-        return self.record['Dpkts']
-
-    @property
-    def dbytes(self):
-        return self.record['dbytes']
-
-
-class NbTCP(NBRecord):
+class NbTCP(NetworkProto):
     def __init__(self, **kwargs):
-        super().__init__('NB15_tcp', **kwargs)
-
-    @property
-    def attributes(self):
-        return TCP_ATTR
+        # noinspection PyTypeChecker
+        super().__init__('NB15_tcp', NBTCP, **kwargs)
 
     @property
     def is_valid(self):
-        if self.swin != 255 or self.dwin != 255:
+        if self.record.swin != 255 or self.record.dwin != 255:
             return False
-        if round(self.synack + self.ackdat, 3) != round(self.tcprtt, 3):
+        if round(self.record.synack + self.record.ackdat, 3) != \
+                round(self.record.tcprtt, 3):
             return False
-        if self.dbytes == 0 and (
-                self.Dload != 0 or self.dttl != 0 or
-                self.Djit != 0 or self.Dpkts != 0):
+        if self.record.dbytes == 0 and (
+                self.record.Dload != 0 or self.record.dttl != 0 or
+                self.record.Djit != 0 or self.record.Dpkts != 0):
             return False
         return True
 
 
-class NbUDP(NBRecord):
+class NbUDP(NetworkProto):
     def __init__(self, **kwargs):
-        super().__init__('NB15_udp', **kwargs)
-
-    @property
-    def attributes(self):
-        return UDP_ATTR
+        # noinspection PyTypeChecker
+        super().__init__('NB15_udp', NBUDP, **kwargs)
 
     @property
     def is_valid(self):
-        return self.smeansz * self.Spkts == self.sbytes \
-               and self.dmeansz * self.Dpkts == self.dbytes
+        return self.record.smeansz * self.record.Spkts == \
+               self.record.sbytes \
+               and self.record.dmeansz * self.record.Dpkts == \
+               self.record.dbytes
+
+
+class Validator:
+
+    def __init__(self):
+        self.models = {'NB15_tcp': TCP_MODEL, 'NB15_udp': UDP_MODEL}
+
+    def validate(self, instance: NetworkProto):
+        model = self.models[instance.name]
+        return instance.check(model)
+
+    @staticmethod
+    def random_test(n, proto=None):
+        """Generate and validate N random records"""
+        validator = Validator()
+        item_counter, valid = [], 0
+        for i in range(n):
+            kind = proto if proto else 'tcp' if randint(0, 1) == 0 else 'udp'
+            inst = Generator.generate(kind)
+            item_counter.append(inst.name)
+            valid += 1 if validator.validate(inst) == VALID else 0
+        show('Generated', n)
+        show('Valid', valid)
+        for item in Counter(item_counter).items():
+            show(*item)
 
 
 class Generator:
 
     @staticmethod
-    def generate(instance):
-        if isinstance(instance, NbTCP):
+    def generate(proto):
+        if proto == 'tcp':
             p, q = uniform(0, 1) < 0.5, uniform(0, 1) < 0.5
             ackdat = uniform(0.0, 5.512234)
             synack = uniform(0.0, 4.525272)
-            instance.set_record(
+            return NbTCP(
                 sbytes=randint(0, 14355774),
                 dbytes=0 if q else randint(0, 14657531),
                 sttl=randint(0, 255),
@@ -201,14 +148,13 @@ class Generator:
                 ct_src_dport_ltm=randint(1, 67),
                 ct_dst_sport_ltm=randint(1, 60),
                 ct_dst_src_ltm=randint(1, 67))
-            return instance
-        if isinstance(instance, NbUDP):
+        if proto == 'udp':
             r = uniform(0, 1) < 0.5
             smeansz = randint(0, 1504)
             dmeansz = randint(0, 1500)
             spkts = randint(0, 10646)
             dpkts = randint(0, 11018)
-            instance.set_record(
+            return NbUDP(
                 sbytes=smeansz * spkts if r else randint(0, 14355774),
                 dbytes=dmeansz * dpkts if r else randint(0, 14657531),
                 sttl=randint(0, 255),
@@ -239,35 +185,6 @@ class Generator:
                 ct_src_dport_ltm=randint(1, 67),
                 ct_dst_sport_ltm=randint(1, 60),
                 ct_dst_src_ltm=randint(1, 67))
-            return instance
-
-
-class Validator:
-
-    def __init__(self):
-        self.models = {'NB15_tcp': TCP_MODEL, 'NB15_udp': UDP_MODEL}
-
-    def validate(self, instance: NetworkProto):
-        model = self.models[instance.name]
-        return instance.check(model)
-
-    @staticmethod
-    def random_test(n, proto=None):
-        """Generate and validate N random records"""
-        validator = Validator()
-        item_counter, valid = [], 0
-        kind = lambda: (
-            NbUDP() if proto == 'udp' else NbTCP() if proto == 'tcp'
-            else NbTCP() if randint(0, 1) == 0 else NbUDP())
-        for i in range(n):
-            inst = Generator.generate(kind())
-            item_counter.append(inst.name)
-            valid += 1 if validator.validate(inst) == VALID else 0
-
-        show('Generated', n)
-        show('Valid', valid)
-        for item in Counter(item_counter).items():
-            show(*item)
 
 
 if __name__ == '__main__':
